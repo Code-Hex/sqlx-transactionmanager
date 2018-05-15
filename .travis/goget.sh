b57b781a71b11be0ce7dcc844bea57f84dd8dde6
K <x00.x7f@gmail.com>
2018-05-15 20:17:13 +0900

    Modified travis

--- .travis/goget.sh
+++ .travis/goget.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ev
 
-go get golang.org/x/tools/cmd/cover
-go get github.com/mattn/goveralls
-go get github.com/go-sql-driver/mysql
-go get github.com/jmoiron/sqlx
-github.com/mattn/go-sqlite3
+go get -v golang.org/x/tools/cmd/cover
+go get -v github.com/mattn/goveralls
+go get -v github.com/lib/pq
+go get -v github.com/go-sql-driver/mysql
+go get -v github.com/jmoiron/sqlx
+go get -v github.com/mattn/go-sqlite3
