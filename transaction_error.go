1c76aad13b2f5fdb67ce7daee248a35a83e2d54c
K <x00.x7f@gmail.com>
2018-05-19 23:22:24 +0900

    Added more comment for error

--- transaction_error.go
+++ transaction_error.go
@@ -5,12 +5,16 @@ const (
 	beginTxErrMsg = "Trying to start a transaction in nested state"
 )
 
+// NestedCommitErr is an error type to notice that
+// commit in nested transaction.
 type NestedCommitErr struct{}
 
 func (n *NestedCommitErr) Error() string {
 	return commitErrMsg
 }
 
+// NestedBeginTxErr is an error type to notice that
+// restart transaction in already begun transaction.
 type NestedBeginTxErr struct{}
 
 func (n *NestedBeginTxErr) Error() string {
